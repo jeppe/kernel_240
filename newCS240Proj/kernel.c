fd079759e6952e0b8d52789eafb8cb397812ac7e
jeppe <coolluyu@gmail.com>
2016-11-24 22:30:54 +0300

    conflict fix

---
+++ newCS240Proj/kernel.c
@@ -0,0 +1,26 @@
+//
+//  kernel.c
+//  Test240
+//
+//  Created by Mootaz N. Elnozahy on 11/12/16.
+//  Copyright © 2016 Mootaz N. Elnozahy. All rights reserved.
+//
+
+#include "kernel.h"
+
+void kernel_start()
+{
+    start_console();
+    initialize_memory();
+    start_disk();
+    ivec_init();
+    
+    
+    char a[80];
+
+    sprintf(a, "Hello World, disk, disk \n");
+
+    write_console((unsigned) strlen(a), a);
+    
+    shutdown_machine();
+}
